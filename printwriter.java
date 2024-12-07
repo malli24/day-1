2c8103020610136890036a685d1373e9772e8993
malli24 <malleswari82006@gmail.com>
2024-12-07 16:26:29 +0530

    Add files via upload

---
+++ printwriter.java
@@ -0,0 +1,14 @@
+import java.io.PrintWriter;
+public class printwriter
+{
+    public static void main(String arhs[])
+    {
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Hello,World");
+        pw.print("This is a");
+        pw.print("print statement.");
+        pw.close();
+    }
+
+
+}
---
+++ type.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class type
+{
+    public static void main(String args[])
+    {
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
