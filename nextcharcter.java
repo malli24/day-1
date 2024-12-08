9cb3e1c477d72a0c87aee5425ccb80531ca5e5a0
malli24 <malleswari82006@gmail.com>
2024-12-08 09:45:28 +0530

    Add files via upload

---
+++ nextcharcter.java
@@ -0,0 +1,12 @@
+class nextcharcter
+{
+    public static void main(String args[])
+    {
+        char c='A';
+        int b=c;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+
+    }
+}
